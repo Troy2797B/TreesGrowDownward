23431aab3a01400f0c36a425cb19f83f7eccd242
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-12-01 15:53:30 -0500

    started it at least... it is confusing

--- src/main/java/TreeOneZero.java
+++ src/main/java/TreeOneZero.java
@@ -4,5 +4,40 @@
 public class TreeOneZero {
     Node root;
 
+    private Node addRecursive(Node current, Integer value){
+        //if the current Node is null
+        if(current == null){
+            //we insert a new node in that position
+            return new Node(value);
+        }
+        //if the new node's value is lower than the current node's,
+        if(current.value > value){
+           //we make our new value the left side of the next value
+            current.left = addRecursive(current.left,value);
+        } else if (current.value < value) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            //the value is already there dummy
+            return current;
+        }return current;
+    }
 
+    //the add method sets the root equal to addRecursive, which returns the current node
+    public void add (Integer value){
+        this.root = addRecursive(root , value);
+    }
+    private boolean containsNodeRecursive(Node current, Integer value){
+        if (current == null){
+            return false;
+        }
+        if(current.value.equals(value)){
+            return true;
+        }
+        if (current.value > value){
+            current.left = containsNodeRecursive(current.left, value);
+            return false;
+        } else if (current.value < value){
+            current.right = containsNodeRecursive(current.right, value);
+        }
+    }
 }
